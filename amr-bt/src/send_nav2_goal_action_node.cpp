554d0adab4a1bfe0f2cb02069f6afc940d6f8768
KroNton <kyrlosfekry@gmail.com>
2026-03-22 19:50:28 +0200

    Add send_nav2_goal_action_node and update CMake and package files

--- amr-bt/src/send_nav2_goal_action_node.cpp
+++ amr-bt/src/send_nav2_goal_action_node.cpp
@@ -1,50 +1,96 @@
-#include <behaviortree_ros2/bt_action_node.hpp>
-#include "action_tutorials_interfaces/action/fibonacci.hpp"
+#include <chrono>
+#include <memory>
+#include <string>
 
-// let's define these, for brevity
-using Fibonacci = action_tutorials_interfaces::action::Fibonacci;
-using GoalHandleFibonacci = rclcpp_action::ServerGoalHandle<Fibonacci>;
+#include <behaviortree_ros2/bt_action_node.hpp>
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+using NavigateToPose  = nav2_msgs::action::NavigateToPose;
+using GoalHandle      = rclcpp_action::ClientGoalHandle<NavigateToPose>;
 
 using namespace BT;
 
-class FibonacciAction: public RosActionNode<Fibonacci>
+class SendWaypointAction: public RosActionNode<NavigateToPose>
 {
+
+private:
+  float goal_pose_x_,goal_pose_y_,goal_pose_yaw_;
+
 public:
-  FibonacciAction(const std::string& name,
+  SendWaypointAction(const std::string& name,
                   const NodeConfig& conf,
                   const RosNodeParams& params)
-    : RosActionNode<Fibonacci>(name, conf, params)
-  {}
+    : RosActionNode<NavigateToPose>(name, conf, params)
+  {
+
+  }
 
   // The specific ports of this Derived class
   // should be merged with the ports of the base class,
   // using RosActionNode::providedBasicPorts()
   static PortsList providedPorts()
   {
-    return providedBasicPorts({InputPort<unsigned>("order")});
+    return providedBasicPorts({
+      InputPort<float>("pose_x"),
+      InputPort<float>("pose_y"),
+      InputPort<float>("pose_yaw"),
+    
+    });
   }
 
+  geometry_msgs::msg::PoseStamped make_goal_pose(float pose_x, float pose_y, float pose_yaw )
+  {
+    geometry_msgs::msg::PoseStamped pose;
+    pose.header.stamp    = now();
+    pose.header.frame_id = "map";
+
+    pose.pose.position.x = pose_x;
+    pose.pose.position.y = pose_y;
+    pose.pose.position.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0.0, 0.0, pose_yaw * M_PI / 180.0);
+    pose.pose.orientation = tf2::toMsg(q);
+
+    return pose;
+  }
   // This is called when the TreeNode is ticked and it should
   // send the request to the action server
   bool setGoal(RosActionNode::Goal& goal) override 
   {
     // get "order" from the Input port
-    getInput("order", goal.order);
+    getInput("pose_x", goal_pose_x_);
+    getInput("pose_y", goal_pose_y_);
+    getInput("pose_yaw", goal_pose_yaw_);
+    
+    goal.pose = make_goal_pose(goal_pose_x_,goal_pose_y_,goal_pose_yaw_);
+    
     // return true, if we were able to set the goal correctly.
     return true;
   }
   
+    NodeStatus onFeedback(const std::shared_ptr<const Feedback> feedback)
+  {
+    RCLCPP_INFO(logger(),
+      "[feedback] dist to goal: %.2f m | ETA: %.1f s | recoveries: %u",
+      feedback->distance_remaining,
+      feedback->estimated_time_remaining.sec +
+        feedback->estimated_time_remaining.nanosec * 1e-9,
+      feedback->number_of_recoveries);
+    return NodeStatus::RUNNING;
+  }
   // Callback executed when the reply is received.
   // Based on the reply you may decide to return SUCCESS or FAILURE.
   NodeStatus onResultReceived(const WrappedResult& wr) override
   {
-    std::stringstream ss;
-    ss << "Result received: ";
-    for (auto number : wr.result->sequence) {
-      ss << number << " ";
+    if (wr.code == rclcpp_action::ResultCode::SUCCEEDED) {
+      RCLCPP_INFO(logger(), " Goal SUCCEEDED!");
+      return NodeStatus::SUCCESS;
     }
-    RCLCPP_INFO(logger(), ss.str().c_str());
-    return NodeStatus::SUCCESS;
   }
 
   // Callback invoked when there was an error at the level
@@ -65,14 +111,5 @@ public:
   // the action, and consider the TreeNode completed.
   // In that case, return SUCCESS or FAILURE.
   // The Cancel request will be send automatically to the server.
-  NodeStatus onFeedback(const std::shared_ptr<const Feedback> feedback)
-  {
-    std::stringstream ss;
-    ss << "Next number in sequence received: ";
-    for (auto number : feedback->partial_sequence) {
-      ss << number << " ";
-    }
-    RCLCPP_INFO(logger(), ss.str().c_str());
-    return NodeStatus::RUNNING;
-  }
+
 };
