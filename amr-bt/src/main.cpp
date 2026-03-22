e5d2714e78f8ee2ce1a835de5c068a9a45b3c9d1
KroNton <kyrlosfekry@gmail.com>
2026-03-23 01:40:13 +0200

    Add RotateAction registration in main.cpp for behavior tree integration

--- amr-bt/src/main.cpp
+++ amr-bt/src/main.cpp
@@ -3,6 +3,7 @@
 #include "behaviortree_cpp/bt_factory.h"
 
 #include "amr-bt/send_nav2_goal_action_node.hpp"
+#include "amr-bt/rotate_action_node.hpp"
 
 int main(int argc, char **argv)
 {
@@ -15,8 +16,14 @@ int main(int argc, char **argv)
   params.nh = node;
   params.wait_for_server_timeout = std::chrono::milliseconds(10000); // time to wait for server at startup
 
-
   factory.registerNodeType<SendWaypointAction>("SendWaypointAction", params);
+  // RotateAction takes node handle directly
+  factory.registerBuilder<RotateAction>(
+      "RotateAction",
+      [node](const std::string &name, const BT::NodeConfig &conf)
+      {
+        return std::make_unique<RotateAction>(name, conf, node);
+      });
 
   // Load your XML tree
   auto tree = factory.createTreeFromFile("/home/kronton/ros2_ws/src/AMR-BT/amr-bt/behavior_trees/tree.xml");
