dffbc6a34e41b2a087b93b5822a43a1bdbbfda3c
KroNton <kyrlosfekry@gmail.com>
2026-03-23 14:53:19 +0200

    Add initial implementation of security_behavior package with main executable and behavior tree XML

---
+++ security_behavior/src/main.cpp
@@ -0,0 +1,33 @@
+#include "rclcpp/rclcpp.hpp"
+#include "behaviortree_ros2/bt_action_node.hpp"
+#include "behaviortree_cpp/bt_factory.h"
+#include "ament_index_cpp/get_package_prefix.hpp" 
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("amr_bt_node");
+
+  BT::BehaviorTreeFactory factory;
+
+  const std::string pkg_lib =
+    ament_index_cpp::get_package_prefix("amr-bt") + "/lib/";
+
+  factory.registerFromPlugin(pkg_lib + "libsend_nav2_goal_action_node.so");
+  factory.registerFromPlugin(pkg_lib + "librotate_action_node.so");
+
+  // create blackboard first and set values on it
+  auto blackboard = BT::Blackboard::create();
+  blackboard->set("node", node);
+  blackboard->set("action_name", std::string("navigate_to_pose"));
+
+  //  pass blackboard INTO createTreeFromFile — not after
+  auto tree = factory.createTreeFromFile(
+    "/home/kronton/ros2_ws/src/AMR-BT/amr-bt/behavior_trees/tree.xml",
+    blackboard); 
+
+  tree.tickWhileRunning();
+
+  rclcpp::shutdown();
+  return 0;
+}
